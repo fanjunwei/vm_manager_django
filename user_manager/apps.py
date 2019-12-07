e445b3d56ab92a063f0030bc44c909c6a7b0302f
fanjunwei <fanjunwei003@163.com>
2019-12-07 20:49:16 +0800

    增加初始用户

--- user_manager/apps.py
+++ user_manager/apps.py
@@ -3,6 +3,24 @@ from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
+from django.db.models.signals import post_migrate
+
+
+def init_user(*args, **kwargs):
+    from django.contrib.auth.models import User
+
+    if not User.objects.all().exists():
+        user = User()
+        user.username = 'root'
+        user.email = 'root@xxx.com'
+        user.set_password('123456')
+        user.save()
+
 
 class UserManagerConfig(AppConfig):
     name = 'user_manager'
+
+    def ready(self):
+        super(UserManagerConfig, self).ready()
+
+        post_migrate.connect(init_user, sender=self)
